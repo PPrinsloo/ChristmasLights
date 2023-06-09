1e15955af413a6ab988c9f3d53d96efd4428ced6
Petrus Prinsloo <petrus.prinsloo@gosuperscript.com>
2023-06-09 13:35:27 +0200

    remove instructions from main file

--- main.go
+++ main.go
@@ -12,32 +12,6 @@ import (
 	"time"
 )
 
-/**
-Deploy one million lights in a 1000x1000 grid
-Lights in your grid are numbered from 0 to 999 in each direction;
-	the lights at each corner are at 0,0, 0,999, 999,999, and 999,0.
-The instructions include whether to turn on, turn off, or toggle various inclusive ranges given as coordinate pairs.
-Each coordinate pair represents opposite corners of a rectangle, inclusive;
-	a coordinate pair like 0,0 through 2,2 therefore refers to 9 lights in a 3x3 square.
-The lights all start turned off.
-For example:
-turn on 0,0 through 999,999 would turn on (or leave on) every light.
-toggle 0,0 through 999,0 would toggle the first line of 1000 lights, turning off the ones that were on, and turning on the ones that were off.
-turn off 499,499 through 500,500 would turn off (or leave off) the middle four lights.
-
-More examples:
-turn off 660,55 through 986,197
-turn off 341,304 through 638,850
-turn off 199,133 through 461,193
-toggle 322,558 through 977,958
-toggle 537,781 through 687,941
-turn on 226,196 through 599,390
-turn on 240,129 through 703,297
-turn on 317,329 through 451,798
-
-Kata Goal: After following the instructions, how many lights are lit?
-*/
-
 // square struct
 type rect struct {
 	rowStart, columnStart, rowEnd, columnEnd int
